8b8c659b211062e9eb846e5d9c973dbbc56b6448
bear <bearyang1109@gmail.com>
2022-02-04 09:00:18 +0800

    增加customShortCut

---
+++ Assets/Editor/CustomShortCut.cs
@@ -0,0 +1,30 @@
+﻿
+#if UNITY_EDITOR_OSX
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CustomShortCut : Editor {
+
+	[MenuItem("customShortCut/ActiveSelectionsObjs &%a")]
+	static void ActiveSelectionsObjs() { 
+		 
+        //transforms是Selection类的静态字段，其返回的是选中的对象的Transform
+        Transform[] transforms = Selection.transforms;
+
+        
+        //将字典中的信息打印出来
+        foreach (Transform item in transforms)
+        {
+			if(item.gameObject) { 
+			item.gameObject.SetActive(!item.gameObject.activeSelf);
+			}
+        }
+    }
+   
+}
+
+
+#endif
--- Assets/Script/Base/Singleton.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Singleton<T> where T: new()
-{
-    static public T ShareInstance{get;} = new T();
-    
-}
